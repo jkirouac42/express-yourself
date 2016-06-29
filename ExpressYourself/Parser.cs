6ae3d54d3d4ac243bc171c69c90bb4b5db4d9912
jkirouac42 <jkirouac42@gmail.com>
2016-06-29 09:48:51 -0400

    final commit

--- ExpressYourself/Parser.cs
+++ ExpressYourself/Parser.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ExpressYourself
-{
-    public class Parser
+namespace ExpressYourself               //  3 groups all covered Type\:(.*),Title\: (.*),+(.*)
+{                                       // Title\: (.*),+    working code
+    public class Parser                 // working code plus length Title\: (.*),+(.*)
     {
         /// <summary>
         /// Looks for a title in a Media file string.
@@ -15,26 +16,58 @@ namespace ExpressYourself
         /// <returns>the title string if it exists</returns>
         public static string GetTitle(string str)
         {
-            // TODO
-            return "";
+            var titleExpression = new Regex(@"Title\: (.*),+");
+            var match = titleExpression.Match(str);
+            if (!match.Success)
+            {
+                return "Title Not Found";  
+            }
+            else
+            {
+                return match.Groups[1].Value;
+            }
         }
 
         public static string GetType(string str)
         {
-            // TODO
-            return "";
+            var typeExpression = new Regex(@"Type\: (.*),Title\: (.*),");
+            var type = typeExpression.Match(str);
+            if (!type.Success)
+            {
+                return "Type not found";
+            }
+
+            else return type.Groups[1].Value;
         }
 
         public static string GetLength(string str)
         {
-            // TODO
-            return "";
+            var lengthExpression = new Regex(@",Length\: (.*)");
+            var length = lengthExpression.Match(str);
+            if (!length.Success)
+            {
+                return "  ";
+            }
+            else
+            {
+                return length.Groups[1].Value;
+            }
         }
 
         public static bool IsValidLine(string str)
         {
-            // TODO
-            return false;
+
+            var validExpression = new Regex(@"Type\: (.*),Title\: (.*),Length\: (.*)");
+            var valid = validExpression.Match(str);
+            if (!valid.Success)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+                
         }
     }
 }
